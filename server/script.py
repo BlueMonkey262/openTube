f487d6ba412eb221e03ff0f872aa3aaf737b1e32
BlueMonkey26 <eli@gessaman.com>
2025-08-05 19:11:05 -0700

    start work on the api

--- server/script.py
+++ server/script.py
@@ -5,32 +5,27 @@ import yaml
 
 app = FastAPI()
 
-#Get Required Data
-cwd = os.getcwd()
-media_folder = cwd + "/server/media/"
-files = os.listdir(media_folder)
-
-stripped_files = [
-    os.path.splitext(file)[0]  # Remove extension
-    for file in files
-    if file.lower().endswith(".mp4")  # Only handle mp4 files
-]
-
-metadataFiles = [file + ".metadat" for file in stripped_files]
-
-
-
-
-
 @app.get("/list/videos")
 async def list_videos():
+    cwd = os.getcwd()
+    media_folder = cwd + "/server/media/"
+    files = os.listdir(media_folder)
+
+    stripped_files = [
+        os.path.splitext(file)[0]  # Remove extension
+        for file in files
+        if file.lower().endswith(".mp4")  # Only handle mp4 files
+    ]
+
+    metadataFiles = [file + ".metadat" for file in stripped_files]
+
     metaList = []
     for metadataFile in metadataFiles:
         with open(media_folder + metadataFile, "r") as f:
             metadata = yaml.safe_load(f)
             metaList.append(metadata)
 
-    return {"files": metaList}
+    return {"videos": metaList}
 
 @app.get("/get/metadata/{thing}")
 async def get_metadata(thing: str):
